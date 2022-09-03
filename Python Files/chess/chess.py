21bbcd10e25d87892eee25fd9e4ba813076b6760
BigSmoke010 <niceniceytube@gmail.com>
2022-09-03 17:40:23 +0100

    rewrote chess.py

--- Python Files/chess/chess.py
+++ Python Files/chess/chess.py
@@ -1,141 +1,116 @@
-import pygame 
-from pygame import Vector2
+import pygame
 
-class board:
-    def __init__(self) -> None:
-        pass
-    def drawboard(self):
-        firstcol = '#769656'
-        for x in range(cell_number):
-            if x % 2 == 0:
-                for i in range(cell_number):
-                    if i % 2 == 0: 
-                        grasrect = pygame.Rect(i * cell_size, x * cell_size, cell_size, cell_size)
-                        pygame.draw.rect(screen, firstcol, grasrect)
-            else:
-                for i in range(cell_number):
-                    if i % 2 != 0: 
-                        grasrect = pygame.Rect(i * cell_size, x * cell_size, cell_size, cell_size)
-                        pygame.draw.rect(screen, firstcol, grasrect)  
-class king:
-    def __init__(self) -> None:
-        self.vect = Vector2(4,0)
-        self.blackvect = Vector2(4,7)
-        self.king = pygame.image.load('images/Chess_klt45.svg')
-        self.blackking = pygame.image.load('images/Chess_kdt45.svg')
-        self.highlight = pygame.image.load('images/highlight.png')
-        self.rects = []
-        self.rect = pygame.rect.Rect(self.vect.x * cell_size, self.vect.y * cell_size, cell_size, cell_size)
-        self.blackrect = pygame.rect.Rect(self.blackvect.x * cell_size, self.blackvect.y * cell_size, cell_size, cell_size)
-        self.moves = [Vector2(1,0),Vector2(1,1),Vector2(1,-1),Vector2(0,1),Vector2(0,-1),Vector2(-1,0),Vector2(-1,1),Vector2(-1,-1)]
-    def drawwhiteking(self):
-        screen.blit(self.king, self.rect)
-    def drawblackking(self):
-        screen.blit(self.blackking, self.blackrect)
-        
-    def showsuggests(self, vect):
-        if showsugges:
-            self.moves = [Vector2(1,0),Vector2(1,1),Vector2(1,-1),Vector2(0,1),Vector2(0,-1),Vector2(-1,0),Vector2(-1,1),Vector2(-1,-1)]
-            for i in self.moves:
-                for x in peon.vects:
-                    if i.x + i.y != x.x + x.y:
-                        x = vect.x  + i.x
-                        y = vect.y  + i.y
-                        self.highlightrect = pygame.rect.Rect(x * cell_size, y * cell_size , cell_size, cell_size)
-                        self.rects.append(self.highlightrect)
-                        screen.blit(self.highlight, self.highlightrect)
-    def hidesuggests(self):
-        global showsugges
-        for i in self.moves:
-            showsugges = False
-            i.x = self.rect.x
-            i.y = self.rect.y
-    def move(self, vect, rect):
-        global showsugges
-        
-        if vect == self.vect:
-            self.rect = pygame.rect.Rect(rect.x , rect.y , cell_size, cell_size)
-            self.vect = Vector2(rect.x / cell_size, rect.y/cell_size )
-        if vect == self.blackvect:
-            self.blackrect = pygame.rect.Rect(rect.x , rect.y , cell_size, cell_size)
-            self.blackvect = Vector2(rect.x / cell_size, rect.y/cell_size )
-            
-        showsugges = False
-        king.hidesuggests()
-        self.rects.clear()
-        
-        
-        
-class peons:
-    def __init__(self) -> None:
-        self.peon = pygame.image.load('images/Chess_plt45.svg')
-        self.blackpeon = pygame.image.load('images/Chess_pdt45.svg')
-        self.vects = [Vector2(0,1),Vector2(1,1),Vector2(2,1),Vector2(3,1),Vector2(4,1),Vector2(5,1),Vector2(6,1),Vector2(7,1)]
-        self.blackvects = [Vector2(0,6),Vector2(1,6),Vector2(2,6),Vector2(3,6),Vector2(4,6),Vector2(5,6),Vector2(6,6),Vector2(7,6)]
-        self.moves = Vector2(1,0)
-    def drawwhitepeon(self):
-        for i in self.vects:
-            rect = pygame.rect.Rect(i.x * cell_size, i.y * cell_size, cell_size, cell_size)
-            screen.blit(self.peon, rect)
-    def drawblackpeon(self):
-        for i in self.blackvects:
-            blackrect = pygame.rect.Rect(i.x * cell_size, i.y * cell_size, cell_size, cell_size)
-            screen.blit(self.blackpeon, blackrect)
-            
-class main:
-    def __init__(self) -> None:
-        pass
-cell_size = 45
+
+cell_size = 50
 cell_number = 8
-showsugges = False
-
-screen = pygame.display.set_mode((cell_number * cell_size, cell_number * cell_size))
+screen = pygame.display.set_mode((cell_size * cell_number, cell_size * cell_number))
 clock = pygame.time.Clock()
 
-board = board()
-king = king()
-x = king.blackvect
-peon = peons()
-
+whitepeon = pygame.image.load('images/Chess_plt45.svg').convert_alpha()
+blackpeon = pygame.image.load('images/Chess_pdt45.svg').convert_alpha()
+whiteking = pygame.image.load('images/Chess_klt45.svg').convert_alpha()
+blackking = pygame.image.load('images/Chess_kdt45.svg').convert_alpha()
+whitehorse = pygame.image.load('images/Chess_nlt45.svg').convert_alpha()
+blackhorse = pygame.image.load('images/Chess_ndt45.svg').convert_alpha()
+whitetower = pygame.image.load('images/Chess_rlt45.svg').convert_alpha()
+blacktower = pygame.image.load('images/Chess_rdt45.svg').convert_alpha()
+whiteslave = pygame.image.load('images/Chess_blt45.svg').convert_alpha()
+blackslave = pygame.image.load('images/Chess_bdt45.svg').convert_alpha()
+highlightimg = pygame.image.load('images/highlight.png').convert_alpha()
+whitepeonlist = [pygame.Rect(0 * cell_size, 1 *cell_size, cell_size, cell_size),pygame.Rect(1 * cell_size, 1 *cell_size, cell_size, cell_size),pygame.Rect(2 * cell_size, 1 *cell_size, cell_size, cell_size),pygame.Rect(3 * cell_size, 1 *cell_size, cell_size, cell_size),pygame.Rect(4 * cell_size, 1 *cell_size, cell_size, cell_size),pygame.Rect(5 * cell_size, 1 *cell_size, cell_size, cell_size),pygame.Rect(6 * cell_size, 1 *cell_size, cell_size, cell_size),pygame.Rect(7 * cell_size, 1 *cell_size, cell_size, cell_size)]
+blackpeonlist = [pygame.Rect(0 * cell_size, 6 *cell_size, cell_size, cell_size),pygame.Rect(1 * cell_size, 6 *cell_size, cell_size, cell_size),pygame.Rect(2 * cell_size, 6 *cell_size, cell_size, cell_size),pygame.Rect(3 * cell_size, 6 *cell_size, cell_size, cell_size),pygame.Rect(4 * cell_size, 6 *cell_size, cell_size, cell_size),pygame.Rect(5 * cell_size, 6 *cell_size, cell_size, cell_size),pygame.Rect(6 * cell_size, 6 *cell_size, cell_size, cell_size),pygame.Rect(7 * cell_size, 6 *cell_size, cell_size, cell_size)]
 running = True
+showhighlight = False
+pe = any
+stat = 'white'
+peonind = 0
+allowedmoves = []
+def drawboard():
+    firstcol = '#72ae50'
+    for x in range(cell_number):
+        if x % 2 == 0:
+            for i in range(cell_number):
+                if i % 2 == 0: 
+                    grasrect = pygame.Rect(i * cell_size, x * cell_size, cell_size, cell_size)
+                    pygame.draw.rect(screen, firstcol, grasrect)
+        else:
+            for i in range(cell_number):
+                if i % 2 != 0: 
+                    grasrect = pygame.Rect(i * cell_size, x * cell_size, cell_size, cell_size)
+                    pygame.draw.rect(screen, firstcol, grasrect)
+
+    
+
+def highlight(i):
+    global allowedmoves
+    if showhighlight:
+        if stat == 'black':
+            screen.blit(highlightimg,i)
+            y = i.y - 50
+            if i.y == 300:
+                allowedmoves = [pygame.Rect(i.x, y , cell_size,cell_size),pygame.Rect(i.x, y - 50 , cell_size,cell_size) ]
+            else:
+                allowedmoves = [pygame.Rect(i.x, y , cell_size,cell_size)]
+        if stat == 'white':
+            screen.blit(highlightimg,i)
+            y = i.y + 50
+            if i.y == 50:
+                allowedmoves = [pygame.Rect(i.x, y , cell_size,cell_size),pygame.Rect(i.x, y + 50 , cell_size,cell_size) ]
+            else:
+                allowedmoves = [pygame.Rect(i.x, y , cell_size,cell_size)]
+        for i in allowedmoves:
+            pygame.draw.rect(screen,'red',i)
+        
+        
+
 while running:
-    if not showsugges:
-        king.showsuggests(x)
-    screen.fill('#EEEED2')
-    board.drawboard()
+    screen.fill('#FFFFFF')
+    drawboard()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    
-    king.drawwhiteking()
-    king.drawblackking()
-    peon.drawblackpeon()
-    peon.drawwhitepeon()
-    
-    if king.rect.collidepoint(pygame.mouse.get_pos()):
-        if pygame.mouse.get_pressed()[0]:
-            showsugges = True
-            x = king.vect
-            y = king.rect
-            king.showsuggests(x)
-    if king.blackrect.collidepoint(pygame.mouse.get_pos()):
-        if pygame.mouse.get_pressed()[0]:
-            showsugges = True
-            x = king.blackvect
-            y = king.blackrect
-            king.showsuggests(x)
-    
-    if showsugges:
-        king.showsuggests(x)
-    else:
-        showsugges = False
-    
-    for i in king.rects:
-        if i.collidepoint(pygame.mouse.get_pos()):
+            
+    for move in allowedmoves:
+        if move.collidepoint(pygame.mouse.get_pos()):
             if pygame.mouse.get_pressed()[0]:
-                king.move(x, i)
-              
+                if stat == 'black':
+                    blackpeonlist[peonind] = move
+                    stat = 'white'
+                    allowedmoves = []
+                    showhighlight = False
+                elif stat == 'white':
+                    whitepeonlist[peonind] = move
+                    stat = 'black'
+                    allowedmoves = []
+                    showhighlight = False
+                    
+    
+    if stat == 'white':
+        for index, i in enumerate(whitepeonlist):
+            if i.collidepoint(pygame.mouse.get_pos()):
+                if pygame.mouse.get_pressed()[0]:
+                    peonind = index
+                    pe = i
+                    stat = 'white'
+                    showhighlight = True
+                    highlight(pe)
+    if stat == 'black':
+        for index, y in enumerate(blackpeonlist):
+            if y.collidepoint(pygame.mouse.get_pos()):
+                if pygame.mouse.get_pressed()[0]:
+                    peonind = index
+                    pe = y
+                    stat = 'black'
+                    showhighlight = True
+                    highlight(pe)
+    for z in whitepeonlist:
+        screen.blit(whitepeon, z)
+    for i in blackpeonlist:
+        screen.blit(blackpeon, i)
+    highlight(pe)
+    
+    
     pygame.display.update()
-    clock.tick(60)    
+    clock.tick(60)
     
     
